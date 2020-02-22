d39be21c7afd3013222f5d6439947d18aa0428ad
scool <zhangfycz@126.com>
2020-02-22 18:15:22 +0800

    branch2 commit

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+	public static void main(String args[]){
+		System.out.println("Hello World");
+	System.out.println("23");
+	System.out.println("5");
+	System.out.println("6");
+	}
+}
